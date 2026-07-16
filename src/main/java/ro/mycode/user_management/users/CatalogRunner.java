f41d0dfc6c5c78e327d7344f14c2be1870dff017
raresdmt-boop <rrsdmt@gmail.com>
2026-07-16 06:57:07 +0300

    first catalog runner

---
+++ src/main/java/ro/mycode/user_management/users/CatalogRunner.java
@@ -0,0 +1,29 @@
+package ro.mycode.user_management.users;
+
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+import ro.mycode.user_management.users.repository.UserRepository;
+
+@Component
+public class CatalogRunner implements CommandLineRunner {
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Override
+    @Transactional
+    public void run(String... args) throws Exception {
+
+        System.out.println(userRepository.findByEmail("cristian.tudor@gmail.com").get().getEmail());
+        System.out.println(userRepository.findByFirstNameAndLastName("Cristian", "Tudor"));
+        System.out.println(userRepository.findByAgeGreaterThan(25));
+        System.out.println(userRepository.findByAgeBetween(15, 20));
+        System.out.println(userRepository.findByLastNameContainingIgnoreCase("Ste"));
+        System.out.println(userRepository.existsByEmail("cristian.tudor@gmail.com"));
+//        userRepository.deleteByEmail("bogdan.alexandrescu@gmail.com");
+
+        System.out.println(userRepository.findByEmailAndPassword("cristian.tudor@gmail.com", "parola123"));
+    }
+}
---
+++ src/main/java/ro/mycode/user_management/users/repository/UserRepository.java
@@ -0,0 +1,24 @@
+package ro.mycode.user_management.users.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+import ro.mycode.user_management.users.model.User;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User,String> {
+
+    Optional<User> findByEmail(String email);
+    List<User> findByFirstNameAndLastName(String firstName, String lastName);
+    List<User> findByAgeGreaterThan(int age);
+    List<User> findByAgeBetween(int minAge, int maxAge);
+    List<User> findByLastNameContainingIgnoreCase(String lastName);
+    boolean existsByEmail(String email);
+    void deleteByEmail(String email);
+
+    @Query("select u from User u where u.email= :email AND u.password = :password")
+    Optional<User> findByEmailAndPassword(@Param("email") String email, @Param("password") String password);
+}
